281d8ad6a09b1c61f7395c4d07882b6db4c7d6b5
TBOTHA0326 <88052326+TBOTHA0326@users.noreply.github.com>
2023-10-05 15:06:19 +0200

    Add files via upload

---
+++ MovementController.cs
@@ -0,0 +1,78 @@
+﻿using MiniProjectDHL.Models;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using OfficeOpenXml;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace MiniProjectDHL.Controllers
+{
+    public class MovementController : Controller
+    {
+        // Action method to render the view
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Action method to process the form submission
+        [HttpPost]
+        public ActionResult Execute(MovementModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                byte[] excelData = GenerateExcelData(model);
+                // Add backend logic here to process the request and generate the result
+
+                // Simulate a delay to show the cooldown period
+                System.Threading.Thread.Sleep(10000); // 10 seconds
+
+                // Generate Excel data and return it for download
+
+
+                return File(excelData, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "MovementTest.xlsx");
+            }
+
+
+            // If model state is not valid, return to the form
+            return View("Index", model);
+
+        }
+
+        // Implement Excel generation logic
+        private byte[] GenerateExcelData(MovementModel model)
+        {
+            using (var package = new ExcelPackage())
+            {
+                var workbook = package.Workbook;
+
+                var worksheet = workbook.Worksheets.Add("Movement Data");
+
+
+                //Adding Headers
+                worksheet.Cells["A1"].Value = "Movement Origin";
+                worksheet.Cells["B1"].Value = "Movement Destination";
+                worksheet.Cells["C1"].Value = "Movement Name";
+                worksheet.Cells["D1"].Value = "Movement Date";
+
+
+                //Filling in data from the model
+                worksheet.Cells["A2"].Value = model.OriginStation;
+                worksheet.Cells["B2"].Value = model.DestinationStation;
+                worksheet.Cells["C2"].Value = model.MovementName;
+                worksheet.Cells["D2"].Value = model.MovementDate.ToString("yyyy-MM-dd");
+
+                //Save the excel package to byte array
+                using (var stream = new MemoryStream())
+                {
+                    package.SaveAs(stream);
+                    return stream.ToArray();
+                }
+            }
+        }
+    }
+}
---
+++ MovementModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiniProjectDHL.Models
+{
+    public class MovementModel
+    {
+        [Required]
+        [StringLength(3)]
+        [RegularExpression("^[A-Z]{3}$", ErrorMessage = " Please enter 3 capital letters")]
+        public string OriginStation { get; set; }
+
+        [Required]
+        [StringLength(3)]
+        [RegularExpression("^[A-Z]{3}$", ErrorMessage = " Please enter 3 capital letters")]
+        public string DestinationStation { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 5, ErrorMessage = "Please enter 5 to 8 alphanumeric characters")]
+        [RegularExpression("^[a-zA-Z0-9]{5,8}$", ErrorMessage = "Only alphanumeric characters are allowed")]
+        public string MovementName { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime MovementDate { get; set; }
+
+    }
+}
